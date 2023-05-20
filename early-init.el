5c1cbd2a5a98d2b993827a79b5a437a49626c58a
Lufei Wang <wwulfric+code@gmail.com>
2023-05-20 23:40:05 +0800

    first commit

---
+++ early-init.el
@@ -0,0 +1,1 @@
+(setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/current:/opt/homebrew/opt/libgccjit/lib/gcc/current:/opt/homebrew/opt/gcc/lib/gcc/current/gcc/aarch64-apple-darwin22/13")
---
+++ init.el
@@ -0,0 +1,32 @@
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; 下方新增一行
+(global-set-key (kbd "<S-return>") (lambda ()
+				     (interactive)
+				     (end-of-line)
+				     (newline-and-indent)))
+;; 上方新增一行
+(global-set-key (kbd "<C-return>") (lambda ()
+				     (interactive)
+				     (previous-line)
+				     (end-of-line)
+				     (newline-and-indent)))
+;; 删除整行
+(global-set-key (kbd "<s-backspace>") (lambda ()
+				  (interactive)
+				  (kill-whole-line)))
+
+;; load 插件 path
+(defvar emacs-root-dir (file-truename "~/.emacs.d/lisp/"))
+
+(defun add-subdirs-to-load-path (dir)
+  "Recursive add directories to `load-path'."
+  (let ((default-directory (file-name-as-directory dir)))
+    (add-to-list 'load-path dir)
+    (normal-top-level-add-subdirs-to-load-path)))
+(add-subdirs-to-load-path emacs-root-dir)
+
+;; 像素级滚动
+(pixel-scroll-precision-mode 1)
+
